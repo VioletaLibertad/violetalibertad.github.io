bfd483edfab1319957bd7d5c57cecb90129fb44f
Violeta Libertad <morada.libertad@gmail.com>
2019-06-05 21:46:45 -0400

    eslint added to manual config

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.scss';
 
-class App extends React.Component {
-    render () {
-        return <h1>Hello World from my own configuration</h1>;
-    }
-}
+const App = () => {
+  return(
+    <h1>Hello World from my own configuration</h1>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
